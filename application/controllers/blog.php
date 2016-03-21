0a387929621f36a333429d569bc09e8312347bd4
Leandro <lehnihon@hotmail.com>
2016-03-21 00:34:51 -0300

    profile

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -6,6 +6,7 @@ class Blog extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
+        $this->output->enable_profiler(TRUE);
         echo "<h1>header</h1>";
         
     }
